242711aee16aad8afce531c38f468dbdcab75698
Tan Jun Ming <jtan@csumb.edu>
2020-02-24 20:17:56 -0800

    Add initial stuff

---
+++ Assets/Scripts/CameraPostProcessing.cs
@@ -0,0 +1,17 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[ExecuteInEditMode]
+public class CameraPostProcessing : MonoBehaviour
+{
+    public Material material;
+
+    void OnRenderImage(RenderTexture source, RenderTexture destination)
+    {
+        // Copy the source Render Texture to the destination,
+        // applying the material along the way.
+
+        Graphics.Blit(source, destination, material);
+    }
+}
